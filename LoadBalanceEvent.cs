91d56f49f54bc7096903e46178897323ed93fa79
Anthony H <anthony@hallmedia.tv>
2018-05-29 10:59:31 +0100

    first draft

---
+++ LoadBalanceEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using Umbraco.Core;
+using Umbraco.Core.Sync;
+
+namespace Hallmedia.Umbraco.Loadbalance
+{
+    public class LoadBalanceEvent : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            //This should be executed on your master server
+            var serverIsMaster = bool.Parse(ConfigurationManager.AppSettings["serverIsMaster"]);
+
+            if (serverIsMaster)
+                ServerRegistrarResolver.Current.SetServerRegistrar(new MasterServerRegistrar());
+            else
+                ServerRegistrarResolver.Current.SetServerRegistrar(new FrontEndReadOnlyServerRegistrar());
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+        }
+    }
+}
---
+++ MasterServerRegistrar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Sync;
+
+namespace Hallmedia.Umbraco.Loadbalance
+{
+    public class MasterServerRegistrar : IServerRegistrar2
+    {
+        public IEnumerable<IServerAddress> Registrations => Enumerable.Empty<IServerAddress>();
+
+        public ServerRole GetCurrentServerRole()
+        {
+            return ServerRole.Master;
+        }
+
+        public string GetCurrentServerUmbracoApplicationUrl()
+        {
+            //NOTE: If you want to explicitly define the URL that your application is running on,
+            // this wil be used for the server to communicate with itself, you can return the 
+            // custom path here and it needs to be in this format:
+            // http://www.mysite.com/umbraco
+
+            return null;
+        }
+    }
+
+    public class FrontEndReadOnlyServerRegistrar : IServerRegistrar2
+    {
+        public IEnumerable<IServerAddress> Registrations => Enumerable.Empty<IServerAddress>();
+
+        public ServerRole GetCurrentServerRole()
+        {
+            return ServerRole.Slave;
+        }
+
+        public string GetCurrentServerUmbracoApplicationUrl()
+        {
+            return null;
+        }
+    }
+}
